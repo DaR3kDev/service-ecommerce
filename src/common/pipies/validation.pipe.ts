231fe4f96da6286a77ce296b9f3596e5c0fd270f
DaR3kDev <villegasdeveloper@outlook.es>
2025-09-30 21:07:36 -0500

    feat: add pagination and response handling with DTOs and interfaces; implement global validation and exception filters

---
+++ src/common/pipies/validation.pipe.ts
@@ -0,0 +1,11 @@
+import { Injectable, ValidationPipe as NestValidationPipe } from '@nestjs/common';
+
+@Injectable()
+export class ValidationPipe extends NestValidationPipe {
+  constructor() {
+    super({
+      transform: true,
+      whitelist: true,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
   app.use(helmet());
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+
+  app.enableShutdownHooks();
 
   await app.listen(process.env.PORT ?? 3000);
 }
